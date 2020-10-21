48365671e5855a13d6cb466faace9a2f25b62971
dreambo8563 <dreambo8563@outlook.com>
2020-10-21 14:14:28 +0800

    add hotreload

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, guard, web, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 
 #[get("/index")]
 async fn index() -> impl Responder {
@@ -12,7 +13,8 @@ async fn index2() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new()
             .service(
                 // prefixes all resources and routes attached to it...
@@ -22,13 +24,18 @@ async fn main() -> std::io::Result<()> {
             )
             .service(
                 web::scope("/api/v2")
+                    // guard for this service
                     .guard(guard::Header("xx", "users.rust-lang.org"))
                     .service(index2),
             )
             // default route
             .default_service(web::to(|| HttpResponse::NoContent()))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    });
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:3000")?
+    };
+
+    server.run().await
 }
