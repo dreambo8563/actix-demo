b6c5cf6165d21a135c216a7136973b5be6513510
dreambo8563 <dreambo8563@outlook.com>
2020-10-21 23:28:43 +0800

    error(QueryConfig): custormized error msg
    * 自定义验证错误, 自定义部分,和原始错误信息结合

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@
 extern crate log;
 
 use actix_web::{error, web, App, HttpResponse, HttpServer};
-
+use serde::Serialize;
 mod controllers;
 mod routes;
+
+#[derive(Serialize)]
+struct MyObj {
+    message: String,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -17,8 +22,15 @@ async fn main() -> std::io::Result<()> {
                 web::QueryConfig::default().error_handler(|err, _| {
                     // <- create custom error response
                     // 定制 QueryConfig 的错误
-                    error::InternalError::from_response(err, HttpResponse::Conflict().finish())
-                        .into()
+                    debug!("{:?}", &err);
+                    let msg = err.to_string();
+                    error::InternalError::from_response(
+                        err,
+                        HttpResponse::Ok().json(MyObj {
+                            message: format!("{}", msg),
+                        }),
+                    )
+                    .into()
                 }),
             )
             .configure(routes::user::routes)
