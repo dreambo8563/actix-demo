f69375646fd487b3659cd783972ce921705092b0
dreambo8563 <dreambo8563@outlook.com>
2020-10-29 20:26:47 +0800

    refactor(run): 封装 run 方法, 减少 pub 部分

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate slog;
 #[macro_use]
 extern crate actix_web;
 use actix_files::Files;
+use actix_web::{App, HttpServer};
 // #[macro_use]
 // extern crate slog;
 // global import macro in the crate
@@ -13,11 +14,10 @@ mod errors;
 mod routes;
 mod utils;
 // use utils::logger;
-use utils::settings;
+// use utils::settings;
 
 use actix_web::{web, HttpResponse, Route};
 
-pub use settings::Settings;
 pub use utils::ENV;
 pub use utils::LOGGING as logger;
 
@@ -26,26 +26,26 @@ pub fn init() {
     env_logger::init();
 }
 /// Route info
-pub struct Routes;
+struct Routes;
 
 impl Routes {
     /// return default route of the app
-    pub fn default() -> Route {
+    fn default() -> Route {
         default_route()
     }
     /// return config for all the app routes
-    pub fn config(cfg: &mut web::ServiceConfig) {
+    fn config(cfg: &mut web::ServiceConfig) {
         debug!(&logger, "route config start");
         routes_config(cfg);
     }
 }
 
 /// base struct of all the configs of Extractor
-pub struct ExtractorConifg;
+struct ExtractorConifg;
 
 impl ExtractorConifg {
     /// customized config for query
-    pub fn query_config() -> actix_web::web::QueryConfig {
+    fn query_config() -> actix_web::web::QueryConfig {
         // change query extractor configuration
         web::QueryConfig::default().error_handler(|err, _| {
             // <- create custom error response
@@ -69,4 +69,17 @@ fn default_route() -> Route {
 /// default route lead to 404 page
 fn static_route(cfg: &mut web::ServiceConfig) {
     cfg.service(Files::new("/", "public/").show_files_listing());
+}
+#[actix_web::main]
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(ExtractorConifg::query_config())
+            .configure(Routes::config)
+            // default route
+            .default_service(Routes::default())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use actix_web::{App, HttpServer};
 use demo::logger;
 use demo::ENV;
 use slog::{info, warn};
@@ -8,22 +7,12 @@ use slog::{info, warn};
 
 //TODO: find a way to supper log rotate
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     demo::init();
 
     let url = &ENV.database.url;
 
     info!(&logger, "env"; "ENV.database"=> format!("{:?}", ENV.database));
     warn!(&logger,"111"; "url" => url);
-    HttpServer::new(|| {
-        App::new()
-            .app_data(demo::ExtractorConifg::query_config())
-            .configure(demo::Routes::config)
-            // default route
-            .default_service(demo::Routes::default())
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    demo::run()
 }
